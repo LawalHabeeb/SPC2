3b546d210fb3eb07a199093ed178f900909d1b4c
Habeeb Lawal <habeeblawal88@gmail.com>
2022-08-15 17:23:38 +0100

    header rebuild start

--- vendor/js/mains.js
+++ vendor/js/mains.js
@@ -47,3 +47,12 @@ setInterval(function() {
 })
     
 }, 4000);
+
+
+window.onload = function (){
+    const menu_btn = document.querySelector('.hamburger');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+    })
+}
